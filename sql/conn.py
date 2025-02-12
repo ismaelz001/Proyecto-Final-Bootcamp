e42759617e939a59ab40c9df677f1d22c83510a4
Ismaelz001 <rodriguezortegaismael@gmial.com>
2025-02-12 08:23:24 +0100

    Inicialización proyecto

---
+++ sql/conn.py
@@ -0,0 +1,18 @@
+import mysql.connector
+from mysql.connector import Error
+
+def conectar():
+    try:
+        conexion = mysql.connector.connect(
+            host ="localhost",
+            user= "root",
+            password = "",
+            database = "Ecommerce"
+        )
+
+        if conexion.is_connected():
+            print("Conexion en verde, todo bien")   
+            return conexion   
+    except Error as e:
+          print(f"Error al conectar a DB: {e}")
+          return None
